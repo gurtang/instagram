d108ad95e62fd83bf97913b7d8f09bbf3375d19b
Slobodan Milosevic <milosevicslobodan@hotmail.com>
2023-06-25 14:02:52 +0200

    Make the feed section responsive

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -4,12 +4,12 @@ import Posts from "@/components/Posts";
 
 function Feed() {
   return (
-    <main>
-      <section className={""}>
+    <main className={"grid grid-col-1 md:grid-cols-3 md:max-w-6xl mx-auto"}>
+      <section className={"md:col-span-2"}>
         <Stories />
         <Posts />
       </section>
-      <section>
+      <section className={"hidden md:inline-grid md:col-span-1"}>
         {/*mini profile */}
         {/*suggestisons*/}
       </section>
