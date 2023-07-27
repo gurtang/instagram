32ac9a16af9785ca401cfddf85319562072bf628
Slobodan Milosevic <milosevicslobodan@hotmail.com>
2023-07-27 17:00:44 +0200

    Added likes

--- src/components/Post.js
+++ src/components/Post.js
@@ -109,6 +109,9 @@ function Post({ img, userImg, caption, id, key, username }) {
 
       {/*post comments */}
       <p className={"p-5 truncate"}>
+        {likes.length > 0 && (
+          <p className={"font-bold mb-1"}>{likes.length} likes</p>
+        )}
         <span className={"font-bold mr-2 "}>{username}</span> {caption}
       </p>
       {comments.length > 0 && (
